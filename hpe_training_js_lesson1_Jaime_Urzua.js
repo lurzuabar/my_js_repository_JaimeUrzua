0913b12bdc57eeae892a447d3ae95fd6e1ecdb91
Luis Jaime Urzua <ljurzua@hotmail.com>
2017-01-19 12:50:42 -0600

    First practice
    Doint the first practices

---
+++ hpe_training_js_lesson1_Jaime_Urzua.js
@@ -0,0 +1,27 @@
+// Exercises 2 and 3
+var text1 = "Hola Mundo";
+var text2 = "Estoy creando un JS";
+var text3 = 5+10;
+var text4 = 9/3;
+var text5 = "funciones matematicas";
+alert(text1);
+alert(text1.length);
+alert(text2);
+alert(text2.length);
+alert(text3);
+alert(text4);
+alert(text5.length);
+
+// Exercise1
+ for (var i=0; i<10;i=i+1) {
+	 alert(i+(". "));
+ }
+ 
+ // Exercise4
+ 
+ // Exercise5
+ var array1 = [];
+ for (var j=0; j<5; j=j+1) {
+	 array1[j]=5*j;
+	 alert(array1[j]);
+ }
