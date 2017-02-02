eaed102168607067eb8de4bea67516b8b2ff2698
Luis Jaime Urzua <ljurzua@hotmail.com>
2017-02-02 16:23:11 -0600

    Practica 3
    Listo!
    Terminado practica 2 y 3

--- hpe_training_js_lesson3_Jaime_Urzua.js
+++ hpe_training_js_lesson3_Jaime_Urzua.js
@@ -1,37 +1,57 @@
-// Exercises 1 
+ // Exercises 1 
 console.log("------ Ejercicio 1 ------");
 console.log("Multiplos de 5: ");
-for (var i=0;i<=100;i=i+5) {
-	console.log("- "+i);
+for (var i=0;i<=100;i+=5) {
+	console.log("* "+i);
 	
 }
 
 
-// Exercise2
+ // Exercise2
 console.log("------ Ejercicio 2 ------");
 var array = ["Contento","Emocionado","Cansado","Dormido","Triste","Feliz"];
-for (i=0;i<array.length;i=i+1) {
+for (i=0;i<array.length;i+=1) {
 	console.log("Entrada "+i+": "+array[i]);
 }
 
 
  // Exercise3
-var array1 = ["Yo","Tu","El","Ella","Nosotros","Ustedes"];
-var array2 = ["Negro","Azul","Blanco","Rojo","Verde"];
-var array3 = ["Cielo","Tierra","Agua","Aire","Fuego"];
-var array4 = ["Campo","Ciudad","Pueblo","Rancho"];
-var array5 = ["Contento","Emocionado","Cansado","Dormido","Triste","Feliz"];
-console.log("Arreglo 1,4: "+" "+array1[4]);
-console.log("Arreglo 2,1: "+" "+array2[1]);
-console.log("Arreglo 3,2: "+" "+array3[2]);
-console.log("Arreglo 1,4: "+" "+array4[3]);
-console.log("Arreglo 5,5: "+" "+array5[5]);
+console.log("------ Ejercicio 3 ------");
+for (i=24;i>=0;i=i-=2) {
+	console.log("* "+i);
+}
   
  
- // Exercise5
-console.log("El residuo de 1500001 entre 5 es: "+50001%5);
-console.log("El residuo de 492290833 entre 66 es: "+492290833%66);
-console.log("El residuo de 3937005 entre 4 es: "+3937005%4);
-console.log("El residuo de 36036036000 entre 360 es: "+36036036000%360);
-console.log("El residuo de 85730755 entre 19 es: "+85730755%19);
-console.log("El residuo de 1670400 entre 9 es: "+1670400%9);
+ // Exercise4
+console.log("------ Ejercicio 4 ------");
+var text = "Sigo programando!!";
+if (text.length > 5) {
+	for (i=10;i<=50;i+=10)
+		console.log("* "+i);
+}
+
+if (text.length < 5) {
+	for (i=10;i<=50;i+=10)
+		console.log("* "+i);
+}
+else {	
+	for (i=50;i>=10;i-=10)
+		console.log("* "+i);
+}
+
+ // Exercise2
+console.log("------ Ejercicio 5 ------");
+var array1 = ["uno","dos","tres","cinco","siete","once","trece"];
+if (array1.length > 5) {
+	for (i=9;i>=0;i-=1)
+		console.log("* "+i);
+}
+
+if (array1.length < 5) {
+	for (i=9;i>=0;i-=1)
+		console.log("* "+i);
+}
+else {	
+	for (i=0;i<=9;i+=1)
+		console.log("* "+i);
+}
